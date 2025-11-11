28ea7bccfffa8085b36e6eb3a61601938d1a317f
tylerwatrich <74198169+tylerwatrich@users.noreply.github.com>
2025-11-10 21:37:15 -0500

    header

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -49,7 +49,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
           <HeaderNav
             data={data}
             onClickLink={closeMenu}
-            className={`absolute md:static left-0 right-0 bg-white dark:bg-gray-950 backdrop-blur-lg md:bg-transparent px-6 md:px-0 pt-2 md:pt-0 pb-4 md:pb-0 transition-all duration-300 z-40
+            className={`absolute md:static left-0 right-0    md:bg-transparent px-6 md:px-0 pt-2 md:pt-0 pb-4 md:pb-0 transition-all duration-300 z-40
               ${isMenuOpen ? 'block' : 'hidden'} md:flex top-full md:top-auto items-center`}
           />
 
