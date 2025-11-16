1bb2c55ab67f2b50f76a45bcf0b784bfd4d06f4e
tylerwatrich <74198169+tylerwatrich@users.noreply.github.com>
2025-11-16 16:13:10 -0500

    test image opt

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -46,7 +46,12 @@ export const Card: React.FC<{
     >
       <div className="relative w-full ">
         {!metaImage && <div className="">No image</div>}
-        {metaImage && typeof metaImage !== 'string' && <Media resource={metaImage} size="33vw" />}
+        {metaImage && typeof metaImage !== 'string' && (
+          <>
+            {console.log('metaImage:', metaImage)}
+            <Media resource={metaImage} size="33vw" />
+          </>
+        )}
       </div>
       <div className="p-4">
         {showCategories && hasCategories && (
